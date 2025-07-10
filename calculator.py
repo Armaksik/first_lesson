7a685cf17f3b5648043e8bbd14d2b8ebb658a3b8
Alexandr <alexalikin28@gmail.com>
2025-07-10 19:54:46 +0500

    исправили калькулятор в питоне

--- calculator.py
+++ calculator.py
@@ -4,21 +4,17 @@ def is_zero(number1):
     else:
         return False
 
-def is_number(number1, number2):
-    if isinstance(number1, int) and isinstance(number2, int):
-        return True
-    else:
-        return False
 
-x = input("введите первое число: ")
-c = input("введите второе число: ")
-if is_number(x,c):
+try:
+    x = input("введите первое число: ")
+    c = input("введите второе число: ")
+    x = int(x)
+    c = int(c)
     print(f"сумма чисел: {x+c}" )
     #print(f"сумма чисел: " , {x+c} )
     #print(f"сумма чисел: " + {x+c} )
     print(f"разность чисел: {x-c}" )
     print(f"умножение чисел: {x*c}" )
-
 
     if is_zero(c):
        print("число с равно 0")
@@ -26,7 +22,7 @@ if is_number(x,c):
         print(f"деление чисел: {x / c}")
         print(f"остаток от деления чисел: {x%c}" )
         print(f"деление без остатка чисел: {x//c}" )
-else:
+except:
     print("одна из переменных не является числом")
 
 
