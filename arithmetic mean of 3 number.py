5063fa4de8da0061880add81ad99c0c5bfa94968
armaksik <mvaganov241@gmail.com>
2025-07-14 08:25:47 +0300

    среднее арифметическое 3-х чисел (немного исправил)

--- arithmetic mean of 3 number.py
+++ arithmetic mean of 3 number.py
@@ -1,8 +1,10 @@
 try:
-    first_number = int(input("введите первое число: "))
-    second_number = int(input("введите второе число: "))
-    third_number = int(input("введите третье число: "))
+    first_number = float(input("введите первое число: "))
+    second_number = float(input("введите второе число: "))
+    third_number = float(input("введите третье число: "))
 
-    print(f"среднее арифметическое 3-х чисел: {(first_number + second_number + third_number)/3} ")
+    sum_nums = (first_number + second_number + third_number)
+
+    print(f"среднее арифметическое 3-х чисел: {(sum_nums/3)} ")
 except:
     print("одна из переменных не является числом")
