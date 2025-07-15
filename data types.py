81fc23a5a077bf4a6ced5f111fe58e0f842b1a12
armaksik <mvaganov241@gmail.com>
2025-07-15 14:38:54 +0300

    типы данных в питоне, которые я знаю

---
+++ data types.py
@@ -0,0 +1,18 @@
+integer = 2
+print(integer, type(integer))
+floating = 3.92
+print(floating, type(floating))
+boolean = True
+print(boolean, type(boolean))
+list = [1, 2, 6]
+print(list, type(list))
+tuple = (1, 2, 6)
+print(tuple, type(tuple))
+set = {1, 2, 6}
+print(set, type(set))
+dict = { "key1": "value1", "key2": "value2"}
+print(dict, type(dict))
+def fun(): pass
+print(fun, type(fun))
+str = ("Hello")
+print(str, type(str))
