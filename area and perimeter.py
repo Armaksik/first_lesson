36c3c115d127cf357424d58e6ad8744f5e318d3f
armaksik <mvaganov241@gmail.com>
2025-07-15 14:13:13 +0300

    совпадающие числа

---
+++ area and perimeter.py
@@ -0,0 +1,8 @@
+try:
+    length = float(input("введите длину фигуры: "))
+    width = float(input("введите ширину фигуры: "))
+
+    print(f"периметр: {(length + width)*2}")
+    print(f"пплощадь: {length * width}")
+except:
+    print("одна из переменных не является числом")
